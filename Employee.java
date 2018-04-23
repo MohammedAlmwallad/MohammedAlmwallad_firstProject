73a93b78c30d5fdec1f2b8594b3396147b7e6aac
MohammedAlmwallad <38670010+MohammedAlmwallad@users.noreply.github.com>
2018-04-23 23:20:14 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -9,18 +9,14 @@ public abstract class Employee {
 		this.id=id;
 		this.name= name;
 	}
-	public Employee(Employee e) {
-		this.id = e.id;
-		this.name = e.name;
-	}
 	public int getid() {
 		
 		return id;
 	}
 	public void display() {
 		
-		System.out.println("id: "+id);
-		System.out.println("name: "+name);
+		System.out.println("id: "+id+"name: "+name);
+		
 	}
 	public abstract  double calculateSalary();
 	
